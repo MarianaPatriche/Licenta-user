8df5288173bc6415c330c9686327a3f2a301c528
mariana.patriche <mariana.patriche@esolutions.ro>
2016-08-02 16:49:43 +0300

    initial commit

---
+++ src/main/java/licenta/user/LicentaUserApplication.java
@@ -0,0 +1,12 @@
+package licenta.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicentaUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicentaUserApplication.class, args);
+	}
+}
